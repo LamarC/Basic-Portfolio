7724e9e77b517694872f5e55c70a0bf54dd670ac
LAPTOP\Diana <dviann89@gmail.com>
2018-10-15 02:22:59 -0500

    Navbar overlaping content

--- assets/JS/logic.js
+++ assets/JS/logic.js
@@ -1,13 +1,4 @@
  //Modal Initialization
- $(document).ready(function () {
-    $(function () {
-
-        //initialize all modals           
-        $('.modal').modal();
-
-        //or by click on trigger
-        $('.trigger-modal').modal();
-
-    }); // end of document ready
+ $(document).ready(function(){
+  $('.carousel').carousel();
 });
-    
