6876da76b6aac5d8dd0d1d33fd533cd1e3ede231
LAPTOP\Diana <dviann89@gmail.com>
2018-10-15 01:17:11 -0500

    Images added to the carousel

--- assets/JS/logic.js
+++ assets/JS/logic.js
@@ -1,5 +1,13 @@
  //Modal Initialization
  $(document).ready(function () {
-  $('.modal').modal();
+    $(function () {
+
+        //initialize all modals           
+        $('.modal').modal();
+
+        //or by click on trigger
+        $('.trigger-modal').modal();
+
+    }); // end of document ready
 });
     
